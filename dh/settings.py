e1ed2634ea12200192b6ab72067707197322ff1f
Anders Hovmöller <anders.hovmoller@dryft.se>
2022-02-05 17:04:03 +0100

    Move to sqlite

--- dh/settings.py
+++ dh/settings.py
@@ -86,11 +86,8 @@ WSGI_APPLICATION = 'dh.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': 'dubbning',
-        'USER': os.environ.get('DOKKU_MYSQL_MYSQL_ENV_MYSQL_USER'),
-        'PASSWORD': os.environ.get('DOKKU_MYSQL_MYSQL_ENV_MYSQL_PASSWORD'),
-        'HOST': os.environ.get('DOKKU_MYSQL_MYSQL_PORT_3306_TCP_ADDR'),
     }
 }
 
